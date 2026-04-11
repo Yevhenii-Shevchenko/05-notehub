274715a4fe6c5b6808a1fb9b121adabbaf652a1e
S.Yevhenii.Serhiiovych <s.yevhenii.shevchenko@gmail.com>
2026-04-11 21:01:20 +0300

    Add App and Search

---
+++ src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import css from "./App.module.css";
+
+function App() {
+  return (
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <p>ПОКа ПУСТО</p>
+        {/* Компонент SearchBox */}
+        {/* Пагінація */}
+        {/* Кнопка створення нотатки */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
